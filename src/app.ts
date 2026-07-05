2bc3f3741f65fb3794e72d67096ed8dcf1f38a78
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-06 01:37:26 +0600

    generate refresh token and access token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import httpStatus from "http-status";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.route";
@@ -20,6 +21,16 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
 app.use("/api/users", userRoutes);
+
+// Not Found Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API Route Not Found",
+    errorDetails: `Cannot ${req.method} ${req.originalUrl}`
+  });
+});
 
 export default app;
