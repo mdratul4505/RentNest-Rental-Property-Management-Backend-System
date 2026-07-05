2188327611b2e6d396a790280c12cd1f659f3ade
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-05 23:51:00 +0600

    added auth router and auth controller and auth services section

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-import { catchAsync } from "../../utils/catchAsynch";
-import { sendResponse } from "../../utils/sendResponce";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
 
