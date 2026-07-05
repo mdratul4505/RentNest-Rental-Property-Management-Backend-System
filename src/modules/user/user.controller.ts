de5badc8efe84e8a421964cceab36550d514c76e
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-05 23:32:20 +0600

    added auth section

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { catchAsync } from "../../utils/catchAsynch";
+import { sendResponse } from "../../utils/sendResponce";
+
+
+
+
+const registerUser = catchAsync( async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+
+    const user = await userService.registerUserIntoDB(payload);
+
+     sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully",
+        data: { user }
+    })
+})
+
+
+
+
+export const userController = {
+    registerUser,
+}
