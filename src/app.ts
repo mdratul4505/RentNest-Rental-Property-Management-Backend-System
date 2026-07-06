4ef8dcc362b7d5fc7c2d682a62ef9cd7e13a320e
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-06 21:32:44 +0600

    feat: mount all routes and register globalErrorHandler in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,13 @@ import httpStatus from "http-status";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.route";
+import { categoryRoutes } from "./modules/category/category.route";
+import { propertyRoutes, landlordPropertyRoutes } from "./modules/property/property.route";
+import { rentalRoutes, landlordRentalRoutes } from "./modules/rentalRequest/rentalRequest.route";
+import { paymentRoutes } from "./modules/payment/payment.route";
+import { reviewRoutes } from "./modules/review/review.route";
+import { adminRoutes } from "./modules/admin/admin.route";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -20,9 +27,17 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
+// Mount Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", userRoutes);
-app.use("/api/auth", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/properties", propertyRoutes);
+app.use("/api/landlord/properties", landlordPropertyRoutes);
+app.use("/api/landlord/requests", landlordRentalRoutes);
+app.use("/api/rentals", rentalRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/admin", adminRoutes);
 
 // Not Found Handler
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -32,5 +47,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     errorDetails: `Cannot ${req.method} ${req.originalUrl}`
   });
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
